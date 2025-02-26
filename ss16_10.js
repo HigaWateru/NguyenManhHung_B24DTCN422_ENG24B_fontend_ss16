2b9593eb1d7ed9ee282c64634e114a5d793fdc4d
Nguyễn Mạnh Hùng <106744156+HigaWateru@users.noreply.github.com>
2025-02-26 14:03:05 +0700

    Update ss16_10.js

--- ss16_10.js
+++ ss16_10.js
@@ -1,3 +1,2 @@
-let str = prompt("Nhập chuỗi bất kì")
-str = str.split(" ")
-document.write(str.length)
+let start=prompt("Nhập số đầu"), end=prompt("Nhập số cuối")
+document.write(`<h1>${Math.floor(Math.random()*(end-start+1)+start)}</h1>`)
